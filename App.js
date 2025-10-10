4d06723501a9aeaa2ed920639681795e75ea48b5
sumit1894 <sumitprasad1971@gmail.com>
2025-10-11 03:15:08 +0530

    3 type of import and Export

--- App.js
+++ App.js
@@ -1,4 +1,23 @@
 
-const add= require("./maths")
+//todo 1st way to import  
+// const add= require("./maths")
 
-console.log(add(2,10))
+//todo 2nd way to import 
+const {add, sub, mult,div, PI}= require("./maths")
+
+console.log(add(10,2))
+console.log(sub(10,2))
+console.log(mult(10,2))
+console.log(div(10,2))
+console.log(PI)
+
+
+
+//todo 3rd way to import 
+const math=require("./maths")
+
+console.log(math.add(10,2))
+console.log(math.sub(10,2))
+console.log(math.mult(10,2))
+console.log(math.div(10,2))
+console.log(math.PI)
--- maths.js
+++ maths.js
@@ -1,5 +1,20 @@
 const add = (a, b) => {
     return a + b;
 }
+//todo 1st way to export;  
+// module.exports=add;
 
-module.exports=add;
+const sub = (a, b) => {
+    return a - b;
+}
+const mult = (a, b) => {
+    return a * b;
+}
+const div = (a, b) => {
+    return a / b;
+}
+const PI=3.124;
+
+
+//todo 2nd way to export  
+module.exports={add, sub, mult,div,PI}; //! use for multiple export
