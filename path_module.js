23478d13f574f965616bd3f794e3ce1e52e120bd
sumit1894 <sumitprasad1971@gmail.com>
2025-10-11 03:56:02 +0530

    path and path module

---
+++ path_module.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+console.log(__dirname);
+console.log(__filename);
+
+
+//! if want to make a new path then it can me done using...
+
+const filePath=path.join("folder","student","data.txt");
+
+console.log(filePath);
+
+const parseData=path.parse(filePath)
+const resolvePath=path.resolve(filePath)
+const extname=path.extname(filePath)
+const basename=path.basename(filePath)
+const dirname=path.dirname(filePath)
+// const seps=path.sep(filePath)
+
+console.log({parseData,resolvePath,extname,basename,dirname});
