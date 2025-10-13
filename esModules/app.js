a96e514bd1371689848f879403278a2092b27686
sumit1894 <sumitprasad1971@gmail.com>
2025-10-13 17:25:42 +0530

    import file

---
+++ esModules/app.js
@@ -0,0 +1,13 @@
+
+
+// import multiply from "../esModules/math.js"
+import {mult,add,sub,div,PI} from "../esModules/math.js"
+import * as math from "../esModules/math.js"
+
+
+console.log(add(10,2))
+console.log(math.sub(10,2))
+console.log(mult(10,2))
+console.log(math.div(10,2))
+console.log(PI)
+
