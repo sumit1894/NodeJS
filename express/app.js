82ebec211519af96b7a84dd224c57499bea18342
sumit1894 <sumitprasad1971@gmail.com>
2025-10-16 17:21:43 +0530

    add .env port and npm run dev using package .json edit

--- express/app.js
+++ express/app.js
@@ -3,12 +3,15 @@ import express from "express";
 
 const app=new express();
 
+
 app.get("/",(req,res)=>{
    return res.send("<h1>Hello World</h1>");
 });
+
 app.get("/about",(req,res)=>{
    return res.send("<h1>Hello About</h1>");
 });
+
 
 const PORT=process.env.PORT || 3000;
 app.listen(PORT,()=>{
