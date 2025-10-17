d8d8aa1154cd25c5b8a3c74000c05d32716d0854
sumit1894 <sumitprasad1971@gmail.com>
2025-10-17 17:36:58 +0530

    req.params,req.params.id getting url data

--- express/app.js
+++ express/app.js
@@ -2,7 +2,6 @@
 import express from "express";
 import { PORT } from "./env.js";
 import path from "path"
-import { fileURLToPath } from "url";
 
 const app = new express();
 
@@ -28,46 +27,71 @@ app.get("/about",(req,res)=>{
 
 //!2 how to access the folder and file.
 
+/*
+
 const __filename = fileURLToPath(import.meta.url)
 console.log(__filename) //! give the file name
 const __dirname = path.dirname(__filename);
 console.log(__dirname) //! give the folder name of this file name
 
 
+*/
 
 
-//todo4 absolute path for static folder post and use both html and css
-const staticPath = path.join(__dirname, "public")
-app.use(express.static(staticPath))
+
+//todo4 absolute path for static folder,to run html and css without res.send
+const staticPath = path.join(import.meta.dirname, "public")
+app.use("/public", express.static(staticPath))
 
 
 
 //todo5 ES Modules using await without async
 
+/* 
+
 const response=await fetch("https://jsonplaceholder.typicode.com/todos/1");
 const data= await response.json()
 console.log(data);
 
+*/
 
 
+//todo Files and url.meta.dirname
+
+/*
 
 app.get("/", (req, res) => {
-   // console.log(import.meta.url) //* only run in vscode
+
    // console.log(import.meta.dirname) //*folder path
    // console.log(import.meta.filename) //*with file Path
 
 
-
    //todo3 to access only index.html
    // const homePagePath = path.join(__dirname, "public", "index.htmls");
-   const homePagePath=path.join(url.meta.dirname,"public","index.html");
-   console.log(homePagePath);
-   res.sendFile(homePagePath);
+   // const homePagePath=path.join(url.meta.dirname,"public","index.html");
+   // res.sendFile(homePagePath);
+
 
 
 })
 
+*/
 
+
+//todo req.params,req.params.id
+
+
+//! single root access paramater
+app.get("/profile/:username", (req, res) => {
+   const userName=req.params.username;
+   res.send(`<h1>Hello,${userName}</h1>`);
+});
+
+//! multiple root acces 
+app.get("/profile/:username/article/:slug",(req,res)=>{
+   const formatedURL=req.params.slug.replace(/-/g, " ");
+   res.send(`<h1> Article ${req.params.username} by ${formatedURL} </h1>`)
+})
 
 
 
